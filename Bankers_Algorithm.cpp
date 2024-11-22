7be6e685c61c0e354607d976edcefa7c5566cf2a
ManOfNeptune27 <Nept@nept.nept>
2024-11-21 20:28:17 -0500

    Changed Banker_Algorithm to be easier to understand for myself

--- Bankers_Algorithm.cpp
+++ Bankers_Algorithm.cpp
@@ -1,106 +1,121 @@
 #include <fstream>
 #include <iostream>
 #include <vector>
+#include <string>
 
-const int NUM_PROCESSES = 5; // Number of processes
-const int NUM_RESOURCES = 3; // Number of resources
+class BankerAlgorithm {
+private:
+    int numProcesses, numResources;
+    std::vector<std::vector<int>> maxDemand, allocation, need;
+    std::vector<int> availableResources;
 
-void calculateNeed(int need[][NUM_RESOURCES], int max[][NUM_RESOURCES], int allocation[][NUM_RESOURCES]) {
-    for (int i = 0; i < NUM_PROCESSES; i++) {
-        for (int j = 0; j < NUM_RESOURCES; j++) {
-            need[i][j] = max[i][j] - allocation[i][j];
+    void calculateNeed() {
+        need.resize(numProcesses, std::vector<int>(numResources, 0));
+        for (int i = 0; i < numProcesses; i++) {
+            for (int j = 0; j < numResources; j++) {
+                need[i][j] = maxDemand[i][j] - allocation[i][j];
+            }
         }
     }
-}
 
-bool isSafe(int available[], int max[][NUM_RESOURCES], int allocation[][NUM_RESOURCES]) {
-    int need[NUM_PROCESSES][NUM_RESOURCES];
-    calculateNeed(need, max, allocation);
+public:
+    BankerAlgorithm(int processes, int resources)
+        : numProcesses(processes), numResources(resources) {}
 
-    bool finish[NUM_PROCESSES] = {false};
-    std::vector<int> safeSequence;
-    int work[NUM_RESOURCES];
-    
-    // Initialize work with available resources
-    for (int i = 0; i < NUM_RESOURCES; i++) {
-        work[i] = available[i];
+    void initialize(const std::string& filename) {
+        std::ifstream inputFile(filename);
+        if (!inputFile) {
+            throw std::runtime_error("Error opening file.");
+        }
+
+        allocation.resize(numProcesses, std::vector<int>(numResources, 0));
+        maxDemand.resize(numProcesses, std::vector<int>(numResources, 0));
+        availableResources.resize(numResources, 0);
+
+        for (int i = 0; i < numProcesses; i++) {
+            for (int j = 0; j < numResources; j++) {
+                inputFile >> allocation[i][j];
+            }
+        }
+        for (int i = 0; i < numProcesses; i++) {
+            for (int j = 0; j < numResources; j++) {
+                inputFile >> maxDemand[i][j];
+            }
+        }
+        for (int i = 0; i < numResources; i++) {
+            inputFile >> availableResources[i];
+        }
+        inputFile.close();
+
+        calculateNeed();
     }
 
-    int completed = 0;
-    while (completed < NUM_PROCESSES) {
-        bool found = false;
-        for (int i = 0; i < NUM_PROCESSES; i++) {
-            if (!finish[i]) {
-                bool canAllocate = true;
-                for (int j = 0; j < NUM_RESOURCES; j++) {
-                    if (need[i][j] > work[j]) {
-                        canAllocate = false;
-                        break;
-                    }
-                }
+    bool isSafe() {
+        std::vector<bool> finish(numProcesses, false);
+        std::vector<int> work = availableResources;
+        std::vector<int> safeSequence;
 
-                if (canAllocate) {
-                    for (int j = 0; j < NUM_RESOURCES; j++) {
-                        work[j] += allocation[i][j];
+        int completed = 0;
+        while (completed < numProcesses) {
+            bool found = false;
+            for (int i = 0; i < numProcesses; i++) {
+                if (!finish[i]) {
+                    bool canAllocate = true;
+                    for (int j = 0; j < numResources; j++) {
+                        if (need[i][j] > work[j]) {
+                            canAllocate = false;
+                            break;
+                        }
                     }
-                    safeSequence.push_back(i);
-                    finish[i] = true;
-                    completed++;
-                    found = true;
+
+                    if (canAllocate) {
+                        for (int j = 0; j < numResources; j++) {
+                            work[j] += allocation[i][j];
+                        }
+                        safeSequence.push_back(i);
+                        finish[i] = true;
+                        completed++;
+                        found = true;
+                    }
                 }
+            }
+
+            if (!found) {
+                std::cout << "System is not in a safe state.\n";
+                return false;
             }
         }
 
-        if (!found) {
-            std::cout << "System is not in a safe state.\n";
-            return false;
+        std::cout << "System is in a safe state.\nSafe sequence is: ";
+        for (size_t i = 0; i < safeSequence.size(); i++) {
+            std::cout << "P" << safeSequence[i];
+            if (i < safeSequence.size() - 1) std::cout << " -> ";
         }
+        std::cout << std::endl;
+        return true;
     }
-
-    std::cout << "System is in a safe state.\nSafe sequence is: ";
-    for (int i = 0; i < safeSequence.size(); i++) {
-        std::cout << "P" << safeSequence[i];
-        if (i < safeSequence.size() - 1) std::cout << " -> ";
-    }
-    std::cout << std::endl;
-
-    return true;
-}
+};
 
 int main() {
-    int available[NUM_RESOURCES];
-    int max[NUM_PROCESSES][NUM_RESOURCES];
-    int allocation[NUM_PROCESSES][NUM_RESOURCES];
+    const int NUM_PROCESSES = 5; // Define number of processes
+    const int NUM_RESOURCES = 3; // Define number of resources
 
-    std::ifstream Input_File("Input_File.txt");
-    if (!Input_File) {
-        std::cerr << "Error opening file.\n";
+    // Create an instance of BankerAlgorithm with the correct parameters
+    BankerAlgorithm banker(NUM_PROCESSES, NUM_RESOURCES);
+
+    // Initialize the banker system and handle errors
+    try {
+        banker.initialize("Input_File.txt");
+    } catch (const std::runtime_error& e) {
+        std::cerr << "Error: " << e.what() << "\n";
         return 1;
     }
 
-    // Read allocation matrix
-    for (int i = 0; i < NUM_PROCESSES; i++) {
-        for (int j = 0; j < NUM_RESOURCES; j++) {
-            Input_File >> allocation[i][j];
-        }
+    // Check if the system is in a safe state
+    if (!banker.isSafe()) {
+        std::cerr << "System is not in a safe state.\n";
+        return 1;
     }
-
-    // Read max matrix
-    for (int i = 0; i < NUM_PROCESSES; i++) {
-        for (int j = 0; j < NUM_RESOURCES; j++) {
-            Input_File >> max[i][j];
-        }
-    }
-
-    // Read available resources
-    for (int i = 0; i < NUM_RESOURCES; i++) {
-        Input_File >> available[i];
-    }
-
-    Input_File.close();
-
-    // Check system safety
-    isSafe(available, max, allocation);
 
     return 0;
 }
